b53be7ead5524f78fd5e769ddc801fffef0d2d71
ibrahim <ibra11002015@gmail.com>
2024-03-29 04:58:28 +0200

    finished the project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function computerChoice() {
+function getComputerChoice() {
     let index = Math.floor(Math.random()*3)+1;
     if (index === 1){
         return 'rock';
@@ -9,4 +9,37 @@ function computerChoice() {
     }
 }
 
-console.log(computerChoice())
+
+
+function playRound(playerChoice, computerChoice) {
+    
+    if ((playerChoice === 'rock' && computerChoice === 'scissors')
+        || (playerChoice === 'paper' && computerChoice === 'rock')
+        || (playerChoice === 'scissors' && computerChoice === 'paper')) {
+            return ('You win! ' + playerChoice + ' beats ' + computerChoice);
+        } else if ((playerChoice === 'rock' && computerChoice === 'paper')
+        || (playerChoice === 'paper' && computerChoice === 'scissors')
+        || (playerChoice === 'scissors' && computerChoice === 'rock')) {
+            return ('You loose! ' + computerChoice + ' beats ' + playerChoice);
+        } else if (playerChoice === computerChoice) {
+            return ('Draw! ' + computerChoice + ' and ' + playerChoice + ' are friends');
+        } else {
+            return 'Make a valid choice!!';
+        }
+
+}
+
+
+function playGame() {
+    let round = 0;
+    
+    while(round <= 4) {
+        let playerChoice = prompt('Chose Rock, paper, or Scissors')
+        let computerChoice = getComputerChoice()
+        console.log(playRound(playerChoice.toLocaleLowerCase(), computerChoice))
+        round++
+    }
+}
+
+
+playGame()
